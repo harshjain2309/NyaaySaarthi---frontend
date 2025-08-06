5cae79977a4960ee4d0c184d1c1c9b9c13365b98
Harsh Jain <135538170+harshjain2309@users.noreply.github.com>
2025-08-07 04:06:29 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import ButtonGradient from "./assets/svg/ButtonGradient";
+import Benefits from "./components/Benefits";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import Hero from "./components/Hero";
+import Services from "./components/Services";
+
+const App = () => {
+  return (
+    <>
+      <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-y-scroll h-screen">
+        <Header />
+
+        <Hero />
+
+        <Services />
+
+        <Benefits />
+
+        <Footer />
+      </div>
+
+      <ButtonGradient />
+    </>
+  );
+};
+
+export default App;
