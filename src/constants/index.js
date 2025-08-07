50eca42eaa8724b510a270a07c729d822c862e93
Harsh Jain <135538170+harshjain2309@users.noreply.github.com>
2025-08-07 16:21:50 +0530

    Add files via upload

---
+++ src/constants/index.js
@@ -0,0 +1,97 @@
+import {
+  benefitIcon1,
+  benefitIcon2,
+  benefitIcon3,
+  benefitIcon4,
+  benefitImage2,
+  file02,
+  homeSmile,
+  plusSquare,
+  searchMd,
+} from "../assets";
+
+
+
+export const heroIcons = [homeSmile, file02, searchMd, plusSquare];
+
+
+
+
+
+
+
+
+
+
+
+
+
+export const benefits = [
+  {
+    id: "0",
+    title: "Instant Answers",
+    text: "Quickly find answers to your queries without sifting through multiple sources.",
+    backgroundUrl: "./src/assets/benefits/card-1.svg",
+    iconUrl: benefitIcon1,
+    imageUrl: benefitImage2,
+  },
+  {
+    id: "1",
+    title: "Enhanced Understanding",
+    text: "Utilizes advanced natural language processing to deliver precise and relevant responses.",
+    backgroundUrl: "./src/assets/benefits/card-2.svg",
+    iconUrl: benefitIcon2,
+    imageUrl: benefitImage2,
+    light: true,
+  },
+  {
+    id: "2",
+    title: "Universal Accessibility",
+    text: "Access the AI chatbot from any device, anywhere, for ultimate convenience.",
+    backgroundUrl: "./src/assets/benefits/card-3.svg",
+    iconUrl: benefitIcon3,
+    imageUrl: benefitImage2,
+  },
+  {
+    id: "3",
+    title: "Rapid Responses",
+    text: "Get quick answers to your questions without delays or lengthy searches.",
+    backgroundUrl: "./src/assets/benefits/card-4.svg",
+    iconUrl: benefitIcon4,
+    imageUrl: benefitImage2,
+    light: true,
+  },
+  {
+    id: "4",
+    title: "Broad Coverage",
+    text: "Explore a wide range of topics and services with the AI chatbot's extensive capabilities.",
+    backgroundUrl: "./src/assets/benefits/card-5.svg",
+    iconUrl: benefitIcon1,
+    imageUrl: benefitImage2,
+  },
+  {
+    id: "5",
+    title: "Daily Improvements",
+    text: "Leverages ongoing advancements in AI to continuously enhance user experience.",
+    backgroundUrl: "./src/assets/benefits/card-6.svg",
+    iconUrl: benefitIcon2,
+    imageUrl: benefitImage2,
+  },
+];
+
+
+
+
+// constants.js
+export const dojServices = [
+  "Know about the divisions of DoJ",
+  "Check judicial appointments (Supreme Court, High Courts, etc.)",
+  "Pendency of cases via National Judicial Data Grid (NJDG)",
+  "Settle your traffic violation",
+  "Live streaming of court cases",
+  "Steps for eFiling and ePay",
+  "Know about Fast Track courts",
+  "Download the eCourts Services Mobile App",
+  "Avail Tele Law services",
+  "Know the current status of your case",
+];
